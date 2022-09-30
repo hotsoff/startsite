961f1609c346dc9a170bc71cbfc3468f51c6eebe
WONDERFIL <hotsoff@icloud.com>
2022-09-30 10:28:45 +0300

    Initial commit

---
+++ bboard/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Bb
+
+class BbAdmin(admin.ModelAdmin) :
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+
+admin.site.register(Bb, BbAdmin)
+
+from .models import Rubric
+admin.site.register(Rubric)
+
+
+# Register your models here.
